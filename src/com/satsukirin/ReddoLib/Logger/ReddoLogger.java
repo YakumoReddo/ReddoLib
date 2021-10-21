5442790a4251972b56ec22dcca711b511a22f4a0
YakumoReddo <3166458362@qq.com>
2021-10-21 14:49:59 +0800

    初始化ReddoLogger Init ReddoLogger

--- src/com/satsukirin/ReddoLib/Logger/ReddoLogger.java
+++ src/com/satsukirin/ReddoLib/Logger/ReddoLogger.java
@@ -1,13 +1,54 @@
 package com.satsukirin.ReddoLib.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+
+import org.bukkit.entity.Player;
 
 import com.satsukirin.ReddoLib.PluginMain;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class ReddoLogger {
+	private static ReddoLogger instance;
 	private Logger logger;
+	private static String ReddoLibPrefix_Console="[ReddoLib] ";
+	private static String ReddoLibPrefix_Player="["+ChatColor.AQUA+"ReddoLib"+ChatColor.WHITE+"] ";
+	private static String ReddoLibPrefix_Warning_Player="["+ChatColor.RED+"Warning"+ChatColor.WHITE+"] "+ChatColor.RED;
+	private List<Player> loggedPlayer;
 	public ReddoLogger() {
+		instance=this;
 		logger = PluginMain.getInstance().getLogger();
+		loggedPlayer=new ArrayList<Player>();
+	}
+	public static ReddoLogger getInstance() {
+		return instance;
+	}
+	public void info(String msg) {
+		logger.info(ReddoLibPrefix_Console+msg);
+		boardcast2Player(msg);
+	}
+	public void warning(String msg) {
+		logger.warning(msg);
+		boardcast2Player(ReddoLibPrefix_Warning_Player+msg);
+	}
+	private void boardcast2Player(String msg) {
+		for(Player p : loggedPlayer) {
+			p.sendMessage(ReddoLibPrefix_Player+msg);
+		}
+	}
+	public void addLogPlayer(Player p) {
+		loggedPlayer.add(p);
+	}
+	public void removeLogPlayer(Player p) {
+		loggedPlayer.remove(loggedPlayer.indexOf(p));
+	}
+	public boolean isLogPlayer(Player p) {
+		return loggedPlayer.contains(p);
+	}
+	public void clearLogPlayer() {
+		loggedPlayer.clear();
 	}
 	
 }
--- src/com/satsukirin/ReddoLib/PluginMain.java
+++ src/com/satsukirin/ReddoLib/PluginMain.java
@@ -2,16 +2,23 @@ package com.satsukirin.ReddoLib;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.satsukirin.ReddoLib.Logger.ReddoLogger;
+
 public class PluginMain extends JavaPlugin {
 	
 	private static PluginMain instance;
-	
+	private ReddoLogger logger;
 	
 	@Override
 	public void onEnable() {
 		PluginMain.instance=this;
+		logger=new ReddoLogger();
+		
 	}
 	
+	public static ReddoLogger getReddoLogger() {
+		return ReddoLogger.getInstance();
+	}
 	public static PluginMain getInstance() {
 		return instance;
 	}
